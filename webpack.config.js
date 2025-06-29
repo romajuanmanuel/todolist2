3aa344471cedc019065a636f0351771d189d98ee
romaxolote <romajuanmanuel@gmail.com>
2025-06-29 09:37:14 -0300

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,29 +3,21 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
-  
+
   return {
-    mode: isProduction ? 'production' : 'development', // Explicit mode setting
+    mode: isProduction ? 'production' : 'development',
     entry: './src/index.js',
     output: {
       filename: isProduction ? '[name].[contenthash].js' : '[name].js',
       path: path.resolve(__dirname, 'dist'),
-      publicPath: '/',
-      clean: true
+      clean: true,
     },
-    devtool: isProduction ? 'source-map' : 'eval-cheap-module-source-map',
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
     module: {
       rules: [
         {
           test: /\.css$/i,
           use: ['style-loader', 'css-loader'],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-          generator: {
-            filename: 'assets/[name][ext]'
-          }
         },
       ],
     },
@@ -33,20 +25,16 @@ module.exports = (env, argv) => {
       new HtmlWebpackPlugin({
         template: './index.html',
         filename: 'index.html',
-        
-      })
+      }),
     ],
     devServer: {
       static: {
-        directory: path.join(__dirname, 'dist'),
+        directory: path.resolve(__dirname, 'dist'),
       },
-      hot: true,
       compress: true,
-      port: 9000,
-      historyApiFallback: true,
-      devMiddleware: {
-      publicPath: '/', // Must match output.publicPath
-    },
+      port: 3000,
+      open: true,
+      hot: true,
     },
     optimization: {
       splitChunks: {
